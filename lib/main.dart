2534162957c478e8d181e43dfc6e6bf5fcd87cf4
ClementPeter <clementpeter2000@gmail.com>
2023-07-08 16:14:01 +0100

    🚀StockPile

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class MyApp extends StatelessWidget {
         //useMaterial3: true,
       ),
       //home: const MyHomePage(),
-      home: const SplashScreen(),
+      //  home: const SplashScreen(),
     );
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,49 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:stockpile/home.dart';
+// import 'package:flutter/material.dart';
+// import 'package:stockpile/home.dart';
 
-//App aplash screen
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+// //App aplash screen
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _navigatetointro();
-  }
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _navigatetointro();
+//   }
 
-  @override
-  //Splash Screen
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'images/stockpile.png',
-              width: 250,
-              height: 250,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+//   @override
+//   //Splash Screen
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: true,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Image.asset(
+//               'images/stockpile.png',
+//               width: 250,
+//               height: 250,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
 
-  //Navigate to Intro Screen PageView
-  void _navigatetointro() async {
-    await Future.delayed(const Duration(milliseconds: 3000), () {});
-    // PageNavigator(ctx: context).nextPageOnly(page: const Intro());
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => MyHomePage(),
-      ),
-    );
-  }
-}
+//   //Navigate to Intro Screen PageView
+//   void _navigatetointro() async {
+//     await Future.delayed(const Duration(milliseconds: 3000), () {});
+//     // PageNavigator(ctx: context).nextPageOnly(page: const Intro());
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => MyHomePage(),
+//       ),
+//     );
+//   }
+// }
